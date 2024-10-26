4775ce9088ac57dfcd4b6de70097b045008242de
chamika <chamikavimukthi1103@gmail.com>
2024-10-26 13:07:17 +0000

    fix error in CICD-1

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end("My name is Chamika. Level 2 Ops Engineer and CICD-1 pipline Update.");
+  res.end("My name is Chamika. Level 2 DevOps Engineer and CICD-1 running !!.");
 });
 
 server.listen(PORT, () => {
